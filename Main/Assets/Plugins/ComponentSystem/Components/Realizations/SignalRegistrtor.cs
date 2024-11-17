f078e71b702cc901961633362dfb68dc138f8559
ivan443250 <100469322+ivan443250@users.noreply.github.com>
2024-11-17 15:12:18 +0400

    Create SignalRegistrtor.cs

---
+++ Main/Assets/Plugins/ComponentSystem/Components/Realizations/SignalRegistrtor.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace ComponentSystem
+{
+    public class SignalRegistrtor : MonoBehaviourComponent
+    {
+        [SerializeField] private MonoScript[] _signalScripts;
+
+        protected override void RegisterSignals(ISignalRegistrator signalRegistrator)
+        {
+            foreach (MonoScript script in _signalScripts)
+            {
+                Type signalType = script.GetClass();
+
+                if (typeof(IDataSignal).IsAssignableFrom(signalType) == false)
+                    throw new Exception($"Script {script.name} is not a representation of the IDataSignal interface");
+
+                MethodInfo methodInfo =
+                    typeof(ISignalRegistrator)
+                    .GetMethod(nameof(ISignalRegistrator.RegisterDataSignal))
+                    .MakeGenericMethod(signalType);
+
+                methodInfo.Invoke(signalRegistrator, new object[0]);
+            }
+        }
+    }
+}
