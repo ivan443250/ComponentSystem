8ae55ad9c935e2a8ee2d754b5c06ae7cb1d4a7aa
ivan443250 <100469322+ivan443250@users.noreply.github.com>
2024-11-22 16:44:04 +0400

    fix build errors

--- Main/Assets/Plugins/ComponentSystem/Components/ComponentInstallers/Realizations/ScriptableComponetsInstaller.cs
+++ Main/Assets/Plugins/ComponentSystem/Components/ComponentInstallers/Realizations/ScriptableComponetsInstaller.cs
@@ -1,25 +1,25 @@
 using System;
-using System.Linq;
-using UnityEditor;
 using UnityEngine;
 
 namespace ComponentSystem
 {
+    //todo
     public class ScriptableComponetsInstaller : MonoBehaviour, IOutsideComponentInstaller
     {
-        [SerializeField] private ScriptableComponentContainer[] _containers;
+        //[SerializeField] private ScriptableComponentContainer[] _containers;
 
         public IComponent[] InstallOutside()
         {
-            return _containers.Select(container => container.ComponentShell).ToArray();
+            //return _containers.Select(container => container.ComponentShell).ToArray();
+            throw new NotImplementedException();
         }
     }
 
-    [Serializable]
-    public class ScriptableComponentContainer
-    {
-        public ScriptableComponent ComponentShell => new(_script.GetClass());
+    //[Serializable]
+    //public class ScriptableComponentContainer
+    //{
+    //    public ScriptableComponent ComponentShell => new(_script.GetClass());
 
-        [SerializeField] private MonoScript _script;
-    }
+    //    [SerializeField] private MonoScript _script;
+    //}
 }
--- Main/Assets/Plugins/ComponentSystem/Components/Realizations/SignalRegistrtor.cs
+++ Main/Assets/Plugins/ComponentSystem/Components/Realizations/SignalRegistrtor.cs
@@ -1,30 +1,29 @@
 using System;
-using System.Reflection;
-using UnityEditor;
-using UnityEngine;
 
 namespace ComponentSystem
 {
+    //todo
     public class SignalRegistrtor : MonoBehaviourComponent
     {
-        [SerializeField] private MonoScript[] _signalScripts;
+        //[SerializeField] private MonoScript[] _signalScripts;
 
         protected override void RegisterSignals(ISignalRegistrator signalRegistrator)
         {
-            foreach (MonoScript script in _signalScripts)
-            {
-                Type signalType = script.GetClass();
+            //foreach (MonoScript script in _signalScripts)
+            //{
+            //    Type signalType = script.GetClass();
 
-                if (typeof(IDataSignal).IsAssignableFrom(signalType) == false)
-                    throw new Exception($"Script {script.name} is not a representation of the IDataSignal interface");
+            //    if (typeof(IDataSignal).IsAssignableFrom(signalType) == false)
+            //        throw new Exception($"Script {script.name} is not a representation of the IDataSignal interface");
 
-                MethodInfo methodInfo =
-                    typeof(ISignalRegistrator)
-                    .GetMethod(nameof(ISignalRegistrator.RegisterDataSignal))
-                    .MakeGenericMethod(signalType);
+            //    MethodInfo methodInfo =
+            //        typeof(ISignalRegistrator)
+            //        .GetMethod(nameof(ISignalRegistrator.RegisterDataSignal))
+            //        .MakeGenericMethod(signalType);
 
-                methodInfo.Invoke(signalRegistrator, new object[0]);
-            }
+            //    methodInfo.Invoke(signalRegistrator, new object[0]);
+            //}
+            throw new NotImplementedException();
         }
     }
 }
