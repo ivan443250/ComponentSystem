b934b48b1250c76bfd1ca89588a262c72e78cd6c
ivan443250 <100469322+ivan443250@users.noreply.github.com>
2024-11-23 01:09:46 +0400

    Add service manager

--- Main/Assets/Plugins/ComponentSystem/SceneEntryPoint.cs
+++ Main/Assets/Plugins/ComponentSystem/SceneEntryPoint.cs
@@ -5,8 +5,12 @@ namespace ComponentSystem
     [RequireComponent(typeof(ComponentContainer))]
     public class SceneEntryPoint : MonoBehaviour
     {
+        [SerializeField] private ServiceManager _serviceManager;
+
         private void Awake()
         {
+            _serviceManager.Initialize(new ComponentGetter(new ComponentDictionary()), new SignalsBranch());
+
             IComponent[] components = GetComponent<ComponentContainer>().InstallOutside();
 
             ComponentSet componentSet = new(components);
---
+++ Main/Assets/Plugins/ComponentSystem/ServiceManager/ServiceManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace ComponentSystem
+{
+    public class ServiceManager : MonoBehaviourComponent
+    {
+        private static ServiceManager _instance;
+
+        protected override void Initialize()
+        {
+            if (_instance != null)
+                throw new System.Exception("There are more than one ServiceManagers on scene");
+
+            _instance = this;
+        }
+
+        public static T GetService<T>() where T : MonoBehaviourComponent
+        {
+            if (_instance.ComponentGetter.TryGetComponent(out T service) == false)
+                throw new System.Exception($"Service {typeof(T)} was not found");
+
+            return service;
+        }
+    }
+}
